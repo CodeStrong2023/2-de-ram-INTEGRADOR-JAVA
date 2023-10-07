54dca9fa97a5575c17615ca2fa56923295d0adf8
Luis Mera <luismera86@gmail.com>
2023-10-07 02:17:47 -0300

    Agregado Clase User, Menus con métdoo customHeaderMenu

---
+++ cine_app/src/users/User.java
@@ -0,0 +1,77 @@
+package users;
+
+public class User {
+    private String name;
+    private String lastName;
+    private int age;
+    private String emai;
+    private String password;
+    private String role;
+    private boolean isActive;
+
+    public User(String name, String lastName, int age, String emai, String password, String role) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.emai = emai;
+        this.password = password;
+        this.role = role;
+        this.isActive = true; // Por defecto todos los usuarios que se crean van a estar activos
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
