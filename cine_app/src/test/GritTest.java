9867470824af92ba7ad27aba7667484736421c3f
Luis Mera <luismera86@gmail.com>
2023-10-22 23:36:59 -0300

    Grid de Usuarios armada en UserGrid2 para testeo

--- cine_app/src/test/GritTest.java
+++ cine_app/src/test/GritTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import grid.UserGrid;
+import grid.UserGrid2;
 import users.UserServices;
 import utils.Grid;
 
@@ -8,8 +9,12 @@ public class GritTest {
     public static void main(String[] args) {
         UserServices.addMockUser();
 //        Grid.userGrid(UserServices.users);
-        UserGrid grid = new UserGrid();
-        grid.generateHeaders();
-        grid.gridUsers(UserServices.users);
+//        UserGrid grid = new UserGrid();
+//        grid.generateHeaders();
+//        grid.gridUsers(UserServices.users);
+        UserServices.users.get(0);
+        UserGrid2 grid2 = new UserGrid2();
+        grid2.generateHeaders();
+        grid2.showUsers(UserServices.users);
     }
 }
